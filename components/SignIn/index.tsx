8d0133cbfcbee9a6d2b9d22dfb9cdbc94ea8d550
omariosman <omarosman23@aucegypt.edu>
2024-11-17 01:01:21 +0200

    :fire: feat: render navbar after success auth

--- components/SignIn/index.tsx
+++ components/SignIn/index.tsx
@@ -1,23 +1,66 @@
 "use client";
 import { signIn, signOut, useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import Image from 'next/image';
 
 export const SignIn = () => {
-  const router = useRouter();
 
   const { data: session } = useSession();
   if (session) {
-    router.push("/home");
-
     return (
       <>
-        Signed in as {session?.user?.name?.slice(0, 10)} <br />
-        <button
-          onClick={() => signOut()}
-          className="text-white py-5 px-10 bg-black" // White text, padding of 20px (py-5 for vertical, px-10 for horizontal)
+        <div style={{
+        display: 'flex',
+        justifyContent: 'space-around',
+        borderTop: '1px solid #ddd',
+        paddingTop: '16px'
+      }}>
+        <div style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          gap: '4px',
+          marginRight: "10px"
+        }}>
+          <Image
+            src="/assets/icons/explore.png?height=24&width=24"
+            alt="Explore icon"
+            width={24}
+            height={24}
+          />
+          <span style={{
+            fontSize: '14px',
+            color: '#1677ff',
+            fontWeight: '500'
+          }}>Explore</span>
+          <div style={{
+            width: '32px',
+            height: '2px',
+            backgroundColor: '#1677ff',
+            borderRadius: '2px'
+          }} />
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            gap: '4px',
+            opacity: 0.5,
+            cursor: 'pointer',
+          }}
         >
-          Sign out
-        </button>
+          <Image
+            src="/assets/icons/daily_story.png?height=24&width=24"
+            alt="Daily Story icon"
+            width={24}
+            height={24}
+          />
+          <span style={{
+            fontSize: '14px',
+            fontWeight: '500'
+          }}>Daily Story</span>
+        </div>
+      </div>
       </>
     );
   } else {
