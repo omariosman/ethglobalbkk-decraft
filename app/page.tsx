d8498e3d3759596809e6e3b57631142e7fa32703
omariosman <omarosman23@aucegypt.edu>
2024-11-17 03:15:48 +0200

    :rocket: fix: depl

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import WelcomeComponent from "@/components/Welcome";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-y-3 bg-white">
-      <PayBlock />
       <WelcomeComponent />
       <SignIn />
     </main>
