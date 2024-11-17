67655ab654314720b7c2313dd7b963f66ed05868
omariosman <omarosman23@aucegypt.edu>
2024-11-17 03:25:10 +0200

    :rocket: fix: depl

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-y-3 bg-white">
       <WelcomeComponent />
+      
       <SignIn />
     </main>
   );
